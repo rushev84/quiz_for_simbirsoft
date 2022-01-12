4166372f199f8266fcd98b82669dc00c64c5e1da
SergeyRushev <rushev84@yandex.ru>
2022-01-12 15:56:41 +0300

    v. 1.2

--- quiz/app/Service/QuizGetService.php
+++ quiz/app/Service/QuizGetService.php
@@ -20,18 +20,18 @@ class QuizGetService
             'questions' => [
                 ['text' => 'Who is known as the father of PHP?', 'choices' => [
                     ['Linus Torvalds', false],
-                    ['Rasmus Lerdorf ---', true],
+                    ['Rasmus Lerdorf', true],
                     ['Brendan Eich', false],
                     ['Bill Gates', false]
                 ]],
                 ['text' => 'Which of the following frameworks was created for PHP-web-artisans?', 'choices' => [
-                    ['Laravel ---', true],
+                    ['Laravel', true],
                     ['React', false],
                     ['Angular', false],
                     ['Bootstrap', false]
                 ]],
                 ['text' => 'How to get the length of a string in PHP?', 'choices' => [
-                    ['strlen() ---', true],
+                    ['strlen()', true],
                     ['strlength()', false],
                     ['stringlen()', false],
                     ['stringlength()', false]
@@ -39,13 +39,13 @@ class QuizGetService
                 ['text' => 'What are the differences between echo and print in PHP?', 'choices' => [
                     ['echo returns a value whereas print does not return a value', false],
                     ['echo does not return a value whereas print returns a value of true', false],
-                    ['echo does not return a value whereas print returns a value of 1  ----', true],
-                    ['echo can accept multiple parameters while print can only take a single argument ---', true]
+                    ['echo does not return a value whereas print returns a value of 1', true],
+                    ['echo can accept multiple parameters while print can only take a single argument', true]
                 ]],
                 ['text' => 'What are the expressions to be used to include and evaluate the specified file?', 'choices' => [
-                    ['include() ---', true],
+                    ['include()', true],
                     ['insert()', false],
-                    ['require() ---', true],
+                    ['require()', true],
                     ['request()', false]
                 ]],
 //                ['text' => '', 'choices' => [
